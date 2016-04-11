ed1ca87dfb0ce17ea42449fa8ce7b7f4fe95d2bc
Jason Kerney <Jason@TeamKerney.com>
2016-04-10 19:45:14 -0700

    reorganized the test

--- test/game_registry_test.exs
+++ test/game_registry_test.exs
@@ -1,10 +1,6 @@
 defmodule TTTTest.Game.Registry do
   use ExUnit.Case, async: true
 
-  test "can create a game registry" do
-    assert {:ok, _pid} = TTT.Game.Registry.start_link()
-  end
-
   setup do
     {:ok, game_registry} = TTT.Game.Registry.start_link()
     {:ok, player_registry} = TTT.Player.Registry.start_link()
@@ -14,6 +10,10 @@ defmodule TTTTest.Game.Registry do
     player1 = TTT.Player.Registry.get_player(player_registry, {"Player1", "Player1_Password"})
     player2 = TTT.Player.Registry.get_player(player_registry, {"Player2", "Player2_Password"})
     {:ok, %{game_registry: game_registry, player1: player1, player2: player2}}
+  end
+
+  test "can create a game registry" do
+    assert {:ok, _pid} = TTT.Game.Registry.start_link()
   end
 
   test "a game registry can create a game for a player", %{game_registry: game_registry, player1: {name, _player_pid} = player} do
