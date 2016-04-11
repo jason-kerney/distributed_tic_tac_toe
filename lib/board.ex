ac208f354b4a56155f98924fe8cf951cbe7da97f
Jason Kerney <Jason@TeamKerney.com>
2016-04-10 19:56:41 -0700

    board starts empty

--- lib/board.ex
+++ lib/board.ex
@@ -1,5 +1,21 @@
 defmodule TTT.Board do
+  @empty_row {:blank, :blank, :blank}
+  @empty_table {@empty_row, @empty_row, @empty_row}
+
+  def empty_row() do
+    @empty_row
+  end
+
+  def empty_table() do
+    @empty_table
+  end
+
   def start_link do
-    Agent.start_link(fn -> %{} end)
+    empty = @empty_table
+    Agent.start_link(fn -> empty end)
+  end
+
+  def get_board(board_pid) do
+    Agent.get(board_pid, fn board -> board end)
   end
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -1,7 +1,18 @@
 defmodule TTTTest.Board do
   use ExUnit.Case, async: true
 
+  setup do
+    {:ok, board_pid} = TTT.Board.start_link()
+    {:ok, board: board_pid}
+  end
+
   test "can create a link to a board" do
-    assert {:ok, pid} = TTT.Board.start_link()
+    assert {:ok, _pid} = TTT.Board.start_link()
+  end
+
+  test "a board starts empty", %{board: board_pid} do
+    empty = TTT.Board.empty_table()
+
+    assert empty == TTT.Board.get_board(board_pid)
   end
 end
