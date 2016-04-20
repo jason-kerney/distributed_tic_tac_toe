5c5abf6b77fa3103c2a1e812cccddb29e88634c3
Jason Kerney <Jason@TeamKerney.com>
2016-04-20 08:14:46 -0700

    refactored 'mark_win'

--- lib/Match.ex
+++ lib/Match.ex
@@ -13,22 +13,21 @@ defmodule TTT.Match do
 
   def mark_win(match_pid, player_pid) do
 
-    {game_registry, {_, p1} = player1, {_, p2} = player2} = state = Agent.get(match_pid,
-      fn  {game_registry, {_, pid1} = p1, {_, pid2} = p2} ->
-          cond do
-            player_pid == pid1 -> {game_registry, p2, p1}
-            player_pid == pid2 -> {game_registry, p1, p2}
-            true -> {:error, nil, nil}
-          end
-        end
-      )
+    {game_registry, {_, pid1} = p1, {_, pid2} = p2} = get_state(match_pid)
+
+    {game_registry, {_, p1} = player1, {_, p2} = player2} = state =
+      cond do
+        player_pid == pid1 -> {game_registry, p2, p1}
+        player_pid == pid2 -> {game_registry, p1, p2}
+        true -> {:error, nil, nil}
+      end
 
     if game_registry == :error do
       :error
     else
       TTT.Game.Registry.stop_game(game_registry, p1)
       Agent.update(match_pid, fn _ -> state end)
-      TTT.Game.Registry.create_game(game_registry, player1, player2) 
+      TTT.Game.Registry.create_game(game_registry, player1, player2)
       :ok
     end
   end
